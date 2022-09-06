91cf69d4894d9eae2bae0b5b37268466bb57c88b
jcrisostomo1 <joshuacr22@gmail.com>
2022-09-06 15:44:22 -0400

    small change to home desc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Josh Crisostomo.</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Full Stack Developer</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer looking to continiously learn while leveraging my current skills 
-            as I gain valuable experience and knowledge in the software development industry
+            as I gain valuable experience and knowledge in the software development industry!
             </p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600'>View Work
