18c704cc69013aa39bcfeb81640a4219e56ce6e5
jcrisostomo1 <joshuacr22@gmail.com>
2022-09-06 15:22:45 -0400

    remove unused import

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import CaclulatorImg from '../assets/calculator.png';
-import WorkImg from '../assets/workImg.jpeg';
 
 const Work = () => {
   return (
@@ -8,18 +7,19 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-green-600'>Work</p>
-          <p className='py-6'>// Check out my recent projects!</p>
+          <p className='py-6'>Check out my recent projects!</p>
         </div>
       
       {/* Container */}
       <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'> 
+
         {/* Grid Item */}
         <div style={{backgroundImage: `url(${CaclulatorImg})`}}
         className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
           {/*Hover Effects*/}
           <div className='opacity-0 group-hover:opacity-100'>
             <span className='text-2xl font-bold text-white tracking-wider'>
-              React Application
+              Javascript Calculator
             </span>
             <div className='pt-8 text-center'>
               <a href='https://jcrisostomo1.github.io/odin-calculator/' rel="noreferrer" target="_blank">
@@ -32,44 +32,6 @@ const Work = () => {
           </div>
         </div>
         
-        {/* Grid Item */}
-        <div style={{backgroundImage: `url(${WorkImg})`}}
-        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-          {/*Hover Effects*/}
-          <div className='opacity-0 group-hover:opacity-100'>
-            <span className='text-2xl font-bold text-white tracking-wider'>
-              React Application
-            </span>
-            <div className='pt-8 text-center'>
-              <a href='/'>
-                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-              </a>
-              <a href='/'>
-                <button  className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        {/* Grid Item */}
-        <div style={{backgroundImage: `url(${WorkImg})`}}
-        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-          {/*Hover Effects*/}
-          <div className='opacity-0 group-hover:opacity-100'>
-            <span className='text-2xl font-bold text-white tracking-wider'>
-              React Application
-            </span>
-            <div className='pt-8 text-center'>
-              <a href='/'>
-                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-              </a>
-              <a href='/'>
-                <button  className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-              </a>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   </div>
